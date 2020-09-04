022b4f7897793dc3bd40e298758c2bd7417b94b5
Challix <55164668+Challix@users.noreply.github.com>
2020-09-04 16:31:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -15,24 +15,40 @@ for(const file of commandFiles){
     client.commands.set(command.name, command);
 }
 
+//Bot initiation sequence
 client.once('ready', () => {
     console.log('Morgana is Online!');
 });
 
+//Creating object
+var discrim = new Object();
+
+//Allows interaction for each message sent
 client.on('message', message =>{
+
+//Looks at individual message authors and reacts to one
+    var person = (message.author.discriminator);
+    //console.log(person);
+    //client.commands.get('react').execute(message, args);
+    //if(person === '5474'){
+    //    message.react('💩');
+    //}
+
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
+
     if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
+        message.channel.send('pong!');
     } else if(command === 'penis'){
-        client.commands.get('penis').execute(message, args);
-    } else if(command === 'poop'){
-        message.react('💩')
+        client.commands.get('size').execute(message, args);
+    } else if(command === 'react'){
+        client.commands.get('react').execute(message, args);
+        //message.react('💩');
     }
 });
 
 
-client.login('Token');
+client.login('NjQ5MDI0ODM0NzU1NDkzOTA4.Xd2xeg.OKurPZgVZf8UxtH6n3x2d0l0zac');
